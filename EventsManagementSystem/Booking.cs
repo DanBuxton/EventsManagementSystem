fffd86c0bca6fecb6d647d6973ec530c6d175998
ROBDAN99 <dan.buxton99@gmail.com>
2018-10-25 20:45:33 +0100

    Add project files.

---
+++ EventsManagementSystem/Booking.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagementSystem
+{
+    public class Booking
+    {
+        private static int _PrevBookingCode { get; set; } = 0;
+        public int BookingCode { get; private set; }
+
+        public int EventCode { get; private set; }
+        public string CustomerName { get; private set; }
+        public string CustomerAddress { get; private set; }
+        public int NumberOfTicketsToBuy { get; private set; }
+        public DateTime DateAdded { get; private set; }
+
+        private double pricePerTicket = 15.35;
+
+        public Booking(int eventCode, string customerName, string customerAddress, int numberOfTicketsToBuy = 1)
+        {
+            //Id's
+            _PrevBookingCode++;
+            BookingCode = _PrevBookingCode;
+            EventCode = eventCode;
+
+            CustomerName = customerName;
+            CustomerAddress = customerAddress;
+            NumberOfTicketsToBuy = numberOfTicketsToBuy;
+
+            DateAdded = DateTime.Now;
+        }
+    }
+}
---
+++ EventsManagementSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagementSystem
+{
+    public class Program
+    {
+        public Stack<Event> Events { get; set; } = new Stack<Event>();
+        public Queue<Booking> Bookings { get; set; } = new Queue<Booking>();
+
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public void CancelBooking()
+        {
+
+        }
+    }
+}
