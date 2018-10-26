d19f5e9bb88b484adcce1c224e771c7978fa15c4
ROBDAN99 <dan.buxton99@gmail.com>
2018-10-26 10:25:16 +0100

    Added transaction class

---
+++ EventsManagementSystem/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsManagementSystem
+{
+    public class Transaction
+    {
+        public enum TransactionType
+        {
+            Add=0,
+            Update=1,
+            Delete=2,
+            Book=3,
+            Cancel=4
+        }
+
+        public TransactionType Type { get; set; }
+
+        public DateTime dateOfTransaction { get; set; } = DateTime.Now;
+
+        public Transaction()
+        {
+
+        }
+    }
+}
