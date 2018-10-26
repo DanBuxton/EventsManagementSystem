db2d206046121d5ebb4dedfdb7d9e243d10799e1
ROBDAN99 <dan.buxton99@gmail.com>
2018-10-26 10:27:28 +0100

    Moved classes into Models folder

--- EventsManagementSystem/Models/Event.cs
+++ EventsManagementSystem/Models/Event.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EventsManagementSystem
+namespace EventsManagementSystem.Models
 {
     public class Event
     {
--- EventsManagementSystem/Program.cs
+++ EventsManagementSystem/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using EventsManagementSystem.Models;
+
 namespace EventsManagementSystem
 {
     public class Program
